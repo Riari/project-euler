9e16d431f85007e4b9892f0f773e73a037948d6a
Riari <3583774+Riari@users.noreply.github.com>
2022-03-24 18:39:08 +0100

    C: Complete solution for 0001

--- c/0001/main.c
+++ c/0001/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello, World!");
-   return 0;
+    int result = 0;
+    for (int i = 3; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            result += i;
+        }
+    }
+
+    printf("%d", result);
 }
