9ad4c27b19c0a2594f3e1d41da0b7487e1d10fc6
Riari <3583774+Riari@users.noreply.github.com>
2022-03-24 19:25:14 +0100

    C: Add alternative solution to 0001

--- c/0001/main.c
+++ c/0001/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+void solution_a() {
     int result = 0;
     for (int i = 3; i < 1000; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
@@ -8,5 +8,19 @@ int main() {
         }
     }
 
-    printf("%d", result);
+    printf("Solution A: %d\n", result);
+}
+
+int find_sum(int k) {
+    int n = 999 / k;
+    return k * (n * (n + 1) / 2);
+}
+
+void solution_b() {
+    printf("Solution B: %d\n", find_sum(3) + find_sum(5) - find_sum(15));
+}
+
+int main() {
+    solution_a();
+    solution_b();
 }
