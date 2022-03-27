64c582931922142181edfaa4fd577b235ff1af90
Riari <3583774+Riari@users.noreply.github.com>
2022-03-27 12:32:54 +0200

    C: Add solution for 0002

---
+++ c/0002/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 1, sum = 0, result = 0;
+    while (result < 4000000) {
+        if (result % 2 == 0) sum += result;
+
+        result = a + b;
+        b = a;
+        a = result;
+    }
+
+    printf("%d", sum);
+}
