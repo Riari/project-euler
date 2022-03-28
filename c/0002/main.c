0025e84f5424cbddb3e4e67d0b2bb4d1fb5bf436
Riari <3583774+Riari@users.noreply.github.com>
2022-03-28 15:56:33 +0100

    C: Set up clang-format

--- c/0002/main.c
+++ c/0002/main.c
@@ -3,7 +3,8 @@
 int main() {
     int a = 1, b = 1, sum = 0, result = 0;
     while (result < 4000000) {
-        if (result % 2 == 0) sum += result;
+        if (result % 2 == 0)
+            sum += result;
 
         result = a + b;
         b = a;
