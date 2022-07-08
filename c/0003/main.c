8b463c8b05026880b2b5bc29f618b9737fa2eb6a
Riari <3583774+Riari@users.noreply.github.com>
2022-07-08 20:22:53 +0100

    C: Inline is_prime in 0003

--- c/0003/main.c
+++ c/0003/main.c
@@ -6,8 +6,8 @@
 
 typedef unsigned long int uli;
 
-bool is_prime(uli n) {
-    for (int i = 2; i <= n / 2; i++) {
+inline bool is_prime(uli n) {
+    for (int i = 3; i <= n / 2; i++) {
         if (n % i == 0)
             return false;
     }
